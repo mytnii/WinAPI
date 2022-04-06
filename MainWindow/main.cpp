db199775a2fd7b52c75e3efc0994984c9f4bfad5
Roman <romanavev@gmal.com>
2022-04-06 21:43:55 +0300

    MainWindow

---
+++ MainWindow/main.cpp
@@ -0,0 +1,77 @@
+#include<Windows.h>
+
+CONST CHAR g_szClassName[] = "My Window Class";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.style = 0;
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hInstance = hInstance;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_szClassName,
+		"Main Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (!hwnd)
+	{
+		MessageBox(NULL, "Window creation failed", "info", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE: break;
+	case WM_COMMAND: break;
+	case WM_CLOSE: 
+		if (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Шо, внатуре?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		{
+			DestroyWindow(hwnd);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
